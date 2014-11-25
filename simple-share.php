a6f0261e0f7ff46e43f0618514416f1f63b5caf1
miya0001 <miya@wpist.me>
2014-11-25 18:56:44 +0900

    some fix

--- simple-share.php
+++ simple-share.php
@@ -46,10 +46,49 @@ class Simple_Share {
 			.simple-share .fb-like iframe {
 				max-width: none;
 			}
+			#simple-share-mobile-footer-wrap,
+			#simple-share-mobile-footer
+			{
+				display: none;
+			}
 			@media screen and (max-width: 480px) {
 				.simple-share
 				{
 					display: none;
+				}
+				#simple-share-mobile-footer-wrap,
+				#simple-share-mobile-footer
+				{
+					height: 40px;
+					display: block;
+				}
+				#simple-share-mobile-footer
+				{
+					width: 100%;
+					height: 40px;
+					position: fixed;
+					bottom: 0px;
+					z-index: 100;
+				}
+				#simple-share-mobile-footer a
+				{
+					color: #ffffff;
+				}
+				#simple-share-mobile-footer .simple-share-mobile-footer-button
+				{
+					display: inline-block;
+					width: 50%;
+					text-align: center;
+					color: #ffffff;
+					line-height: 40px;
+				}
+				#simple-share-mobile-footer .simple-share-twitter
+				{
+					background-color: #00acee;
+				}
+				#simple-share-mobile-footer .simple-share-facebook
+				{
+					background-color: #3b5998;
 				}
 			}
 		</style>
@@ -89,7 +128,7 @@ class Simple_Share {
 		);
 
 		if ( 'ja' === get_locale() ) {
-			$share_buttons['hatena'] = '<a href="http://b.hatena.ne.jp/entry/%1$s" class="hatena-bookmark-button" data-hatena-bookmark-title="%2$s" data-hatena-bookmark-layout="vertical-balloon" data-hatena-bookmark-lang="en"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="https://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
+			$share_buttons['hatena'] = '<a href="http://b.hatena.ne.jp/entry/%1$s" class="hatena-bookmark-button" data-hatena-bookmark-title="%2$s" data-hatena-bookmark-layout="vertical-balloon" data-hatena-bookmark-lang="en"><img src="//b.st-hatena.com/images/entry-button/button-only@2x.png" width="20" height="20" style="border: none;" /></a><script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
 		}
 
 		return apply_filters( 'simple_share_get_share_buttons', $share_buttons );
@@ -108,8 +147,27 @@ class Simple_Share {
 			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
-		<script src="https://apis.google.com/js/platform.js" async defer></script>
+		<script src="//apis.google.com/js/platform.js" async defer></script>
 		<!-- end simple-share -->
 		<?php
+
+		$mobile_footer = '<div id="simple-share-mobile-footer-wrap"></div>';
+		if ( is_singular() ) {
+			$mobile_footer .= '<div id="simple-share-mobile-footer">';
+			$mobile_footer .= '<div class="simple-share-mobile-footer-button simple-share-twitter"><a href="https://twitter.com/intent/tweet?text='.urlencode( esc_attr( get_the_title() ).' '.esc_url( get_permalink() ) ).'">Twitter</a></div>';
+			$mobile_footer .= '<div class="simple-share-mobile-footer-button simple-share-facebook"><a href="https://www.facebook.com/sharer/sharer.php?u='.urlencode( esc_url( get_permalink() ) ).'">facebook</a></div>';
+			$mobile_footer .= '</div>';
+		}
+
+		echo apply_filters( 'simple_share_mobile_footer', $mobile_footer );
+?>
+<script>
+(function($){
+
+	var simnple_share_footer = $('#simple-share-mobile-footer');
+
+})(jQuery);
+</script>
+<?php
 	}
 }
